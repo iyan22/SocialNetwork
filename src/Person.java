d14b553b2d9e7b241b06b2cdab38a028db3e68f0
Iyán Álvarez <iyan.22.alvarez@gmail.com>
2020-09-25 00:10:31 +0200

    Initial menu fixed. Add person function working. Add person to social network from file working.Print out people to file working.

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Person {
@@ -7,11 +8,45 @@ public class Person {
     private String identifier;
     private String name;
     private String surname;
-    private Date birthdate;
+    private String birthdate;
+    private String gender;
+    private String birthplace;
     private String home;
-    private ArrayList<String> studydata;
-    private ArrayList<String> workdata;
-    private ArrayList<String> movies;
+    private String[] studydata;
+    private String[] workdata;
+    private String[] movies;
     private String groupcode;
 
+    public Person(String identifier, String name, String surname, String birthdate, String gender, String birthplace, String home, String[] studydata, String[] workdata, String[] movies, String groupcode) {
+        this.identifier = identifier;       // 0
+        this.name = name;                   // 1
+        this.surname = surname;             // 2
+        this.birthdate = birthdate;         // 3
+        this.gender = gender;               // 4
+        this.birthplace = birthplace;       // 5
+        this.home = home;                   // 6
+        this.studydata = studydata;         // 7
+        this.workdata = workdata;           // 8
+        this.movies = movies;               // 9
+        this.groupcode = groupcode;         // 10
+    }
+
+    @Override
+    public String toString() {
+        String studydataS = "";
+        for(int i = 0; i < studydata.length; i++) {
+            studydataS += studydata[i];
+        }
+        String workdataS = "";
+        for(int i = 0; i < workdata.length; i++) {
+            workdataS += workdata[i];
+        }
+        String moviesS = "";
+        for(int i = 0; i < movies.length; i++) {
+            moviesS += movies[i];
+        }
+        return  identifier + "," + name + "," + surname + "," + birthdate + "," + gender + "," +
+                birthplace + "," + home + "," + studydataS + "," + workdataS +
+                "," + moviesS + "," + groupcode;
+    }
 }
--- src/SocialNetwork.java
+++ src/SocialNetwork.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -13,7 +16,6 @@ public class SocialNetwork {
     }
 
     // Methods
-
     /**
      * Presents an initial menu with the different choices for interacting with the social network.
      */
@@ -28,8 +30,8 @@ public class SocialNetwork {
         System.out.println( "Social Network menu: \n" +
                             "1. Load 'people' into the network ... \n" +
                             "2. Load 'relationships'...\n" +
-                            "3. Print out people \n" +
-                            "4. Search ... \n" +
+                            "3. Print out people to console ... \n" +
+                            "4. Print out people to file ... \n" +
                             "5. Log out \n \n");
         System.out.print("Select one option: ");
     }
@@ -40,27 +42,92 @@ public class SocialNetwork {
     private void selectionInitialMenu() {
         Scanner sc = new Scanner(System.in);
         int option = sc.nextInt();
+        String fn;
         switch (option){
             case 1:
-                System.out.println("Option 1 correct");
+                System.out.println("Write the name of the file: " );
+                fn = sc.next();
+                System.out.print("\nLoading 'people' into the network ... ");
+                addPeopleFromFile(fn);
+                System.out.print("completed.\n\n");
                 break;
             case 2:
-                System.out.println("Option 2 correct");
+                System.out.print("\nLoading 'realtions' ... ");
+
+                System.out.print("completed.\n\n");
                 break;
             case 3:
-                System.out.println("Option 3 correct");
+                System.out.println("Printing out people to console: \n" );
+                printPeopleToConsole();
+                System.out.println("\nCompleted.\n\n");
                 break;
             case 4:
-                System.out.println("Option 4 correct");
+                System.out.println("Write the name of the file: " );
+                fn = sc.next();
+                System.out.print("Printing out people to file ... " );
+                printPeopleToFile(fn);
+                System.out.print("completed.\n\n");
                 break;
             case 5:
-                System.out.println("Option 5 correct");
+                System.out.println("Logging out...");
                 break;
             default:
-                printInitialMenu();
-                selectionInitialMenu();
                 break;
         }
+        if (option != 5) {
+            printInitialMenu();
+            selectionInitialMenu();
+        }
+    }
+
+    public void addPerson(String data) {
+        String[] d = data.split(",");
+        String[] d7 = d[7].split(";");
+        String[] d8 = d[8].split(";");
+        String[] d9 = d[9].split(";");
+        Person p = new Person(d[0], d[1], d[2], d[3], d[4], d[5], d[6], d7, d8, d9, d[10]);
+        personList.add(p);
+    }
+
+    public void addPeopleFromFile(String filename) {
+        File f = new File("files/" + filename);
+        try {
+            Scanner sf = new Scanner(f);
+            String sexample = "idperson,name,lastname,birthdate,gender,birthplace,home,studiedat,workplaces,films,groupcode";
+            if (sexample.equals(sf.nextLine())) {
+                while (sf.hasNextLine()) {
+                    addPerson(sf.nextLine());
+                }
+            }
+            else {
+                System.err.println("Error: File data is not in required format");
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Error: File was not found");
+        }
+    }
+
+    public void printPeopleToConsole() {
+        for (Person p: personList) {
+            System.out.println(p.toString());
+        }
+    }
+
+    public void printPeopleToFile(String filename) {
+        File f = new File("files/" + filename);
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter(f);
+            String sexample = "idperson,name,lastname,birthdate,gender,birthplace,home,studiedat,workplaces,films,groupcode\n";
+            fw.write(sexample);
+            for (Person p: personList) {
+                fw.write(p.toString() + "\n");
+            }
+            fw.close();
+        } catch (IOException e) {
+            System.err.println("Error: File was not found");
+        }
+
     }
 
     public static void main(String[] args) {
