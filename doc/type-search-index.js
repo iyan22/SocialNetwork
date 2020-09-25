4b6e606ad499f3869cdbfe8e92d39ac76599355d
Iyán Álvarez <iyan.22.alvarez@gmail.com>
2020-09-25 11:13:57 +0200

    UML added and .gitignore fixed

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Person"},{"p":"<Unnamed>","l":"SocialNetwork"},{"p":"<Unnamed>","l":"SocialNetworkSimulation"}]
